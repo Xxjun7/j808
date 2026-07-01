89a4d6b7627882ab964e76cd04afa8d0327e3901
Xxjun7 <zx6610200@yahoo.com.tw>
2026-07-02 06:42:08 +0800

    Update script.js
    Refactor load function to directly update innerText from fetched data.

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ const API = "https://cors.j808vip.workers.dev/";
 
 async function load(){
 
-  const res = await fetch(API);
-  const data = await res.json();
+const res = await fetch(API);
+const data = await res.json();
 
-  if(data.error){
-    throw new Error(data.error);
-  }
-
-  cash_buy.innerText = data.cash_buy;
-  cash_sell.innerText = data.cash_sell;
-  spot_buy.innerText = data.spot_buy;
-  spot_sell.innerText = data.spot_sell;
+cash_buy.innerText = data.cash_buy;
+cash_sell.innerText = data.cash_sell;
+spot_buy.innerText = data.spot_buy;
+spot_sell.innerText = data.spot_sell;
 
 }
 
