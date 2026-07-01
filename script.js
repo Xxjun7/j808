ef9cbc29c27adc9864297fd85afc4ca64c965f2f
Xxjun7 <zx6610200@yahoo.com.tw>
2026-07-02 06:36:41 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const API = "https://cors.j808vip.workers.dev/";
+
+async function load(){
+
+  const res = await fetch(API);
+  const data = await res.json();
+
+  if(data.error){
+    throw new Error(data.error);
+  }
+
+  cash_buy.innerText = data.cash_buy;
+  cash_sell.innerText = data.cash_sell;
+  spot_buy.innerText = data.spot_buy;
+  spot_sell.innerText = data.spot_sell;
+
+}
+
+load();
+setInterval(load, 30000);
